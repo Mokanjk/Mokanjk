cf80279fe2e9758eeef022c12fbae1c63ee5f00d
Mokanjk <141749517+Mokanjk@users.noreply.github.com>
2023-08-09 18:34:34 +0200

    1 Step
    the the endles.ende.de

---
+++ myownstartofcoe.py
@@ -0,0 +1,24 @@
+#              Internet                 # Owner
+#  A              B                C    # users
+#  a is :3  /     b:2      /       c :3 # functions
+#A-user is good in: 3 things            # good in
+#C-user is good in: 3 thing .but are different # good in
+#B-User is a gate : in and out          # connection
+#A-user:3.good.things:> are <: math     # math.start
+#A-user:2.good.things:> are <: physics  # math.gravirt
+#A-user:1.good.things:> are <: geometry # math.triangle
+#C-user:3.bad.things:< is <: htam # tras.htam # tras.htam
+#C-user:2.bad.things:< is <: scosyhp    # trivarag.htam
+#C-user:1.bad.things:< is <: ytremoeg   # elgnaitr.htam
+#B-User is a gate : out and in          # connection   # between line.4.6:12.13.enil(exit) # now this would be exit line.13
+#B-User is a gate : o.i from X/x points which is at this moment from line.4 to mac line 14.(exit)
+
+^^^^ all of that is True
+____ all of that is True
+#B-User are gates: from l.1.2.3 == start.midle.end == unless you press enter you wont go on to the next line.nevel you #
+and that is the end of my coding exper.tis if you want more please let me know what do you think
+if you wont understand what i was trying to say here well thats on you / if you want me to code it. into something that
+    can be visible to GUI well contact me and tell me what would you want me to turn this into
+and i will try my best to provide a positive or at least a neutral outcome.
+    cya. C: Users .ayc
+not goodbey
